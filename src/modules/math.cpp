8e99313902b1417f37efedc45743b032be760fd3
zanadoman <domanseven@gmail.com>
2025-02-09 21:20:08 +0100

    std math functions

--- src/modules/math.cpp
+++ src/modules/math.cpp
@@ -27,9 +27,8 @@
 
 #include "../../include/wizard_engine/modules/math.hpp"
 
-#include <SDL3/SDL_stdinc.h>
-
 #include <array>
+#include <cmath>
 #include <cstdint>
 
 namespace wizard_engine::modules {
@@ -43,17 +42,17 @@ void math::set_mt19937_64_seed(std::uint_fast64_t seed) {
 }
 
 auto math::magnitude_2d(const std::array<float, 2>& vector) noexcept -> float {
-  return SDL_sqrtf(SDL_powf(vector[0], 2) + SDL_powf(vector[1], 2));
+  return std::sqrtf(std::powf(vector[0], 2) + std::powf(vector[1], 2));
 }
 
 auto math::magnitude_3d(const std::array<float, 3>& vector) noexcept -> float {
-  return SDL_sqrtf(SDL_powf(vector[0], 2) + SDL_powf(vector[1], 2) +
-                   SDL_powf(vector[2], 2));
+  return std::sqrtf(std::powf(vector[0], 2) + std::powf(vector[1], 2) +
+                    std::powf(vector[2], 2));
 }
 
 auto math::angle_2d(const std::array<float, 2>& vector) noexcept -> float {
   if (static_cast<bool>(vector[0]) || static_cast<bool>(vector[1])) {
-    return SDL_atan2f(vector[1], vector[0]);
+    return std::atan2f(vector[1], vector[0]);
   }
   return 0;
 }
@@ -62,7 +61,8 @@ auto math::angle_3d(const std::array<float, 3>& vector) noexcept
     -> std::array<float, 2> {
   const auto magnitude{magnitude_3d(vector)};
   if (static_cast<bool>(magnitude)) {
-    return {angle_2d({vector[0], vector[1]}), SDL_acosf(vector[2] / magnitude)};
+    return {angle_2d({vector[0], vector[1]}),
+            std::acosf(vector[2] / magnitude)};
   }
   return {0, 0};
 }
@@ -87,42 +87,42 @@ auto math::normalize_3d(const std::array<float, 3>& vector) noexcept
 }
 
 auto math::resolve_x_2d(float magnitude, float angle) noexcept -> float {
-  return magnitude * SDL_cosf(angle);
+  return magnitude * std::cosf(angle);
 }
 
 auto math::resolve_y_2d(float magnitude, float angle) noexcept -> float {
-  return magnitude * SDL_sinf(angle);
+  return magnitude * std::sinf(angle);
 }
 
 auto math::resolve_x_3d(float magnitude,
                         const std::array<float, 2>& angle) noexcept -> float {
-  return magnitude * SDL_sinf(angle[1]) * SDL_cosf(angle[0]);
+  return magnitude * std::sinf(angle[1]) * std::cosf(angle[0]);
 }
 
 auto math::resolve_y_3d(float magnitude,
                         const std::array<float, 2>& angle) noexcept -> float {
-  return magnitude * SDL_sinf(angle[1]) * SDL_sinf(angle[0]);
+  return magnitude * std::sinf(angle[1]) * std::sinf(angle[0]);
 }
 
 auto math::resolve_z_3d(float magnitude,
                         const std::array<float, 2>& angle) noexcept -> float {
-  return magnitude * SDL_cosf(angle[1]);
+  return magnitude * std::cosf(angle[1]);
 }
 
 auto math::transformation_matrix_2d(float angle, float scale) noexcept
     -> std::array<float, 4> {
-  const auto cosine_scale{SDL_cosf(angle) * scale};
-  const auto sine_scale{SDL_sinf(angle) * scale};
+  const auto cosine_scale{std::cosf(angle) * scale};
+  const auto sine_scale{std::sinf(angle) * scale};
   return {cosine_scale, -sine_scale, sine_scale, cosine_scale};
 }
 
 auto math::transformation_matrix_3d(const std::array<float, 2>& angle,
                                     float scale) noexcept
     -> std::array<float, 9> {
-  const auto cosine_theta{SDL_cosf(angle[0])};
-  const auto sine_theta{SDL_sinf(angle[0])};
-  const auto cosine_phi{SDL_cosf(angle[1])};
-  const auto sine_phi{SDL_sinf(angle[1])};
+  const auto cosine_theta{std::cosf(angle[0])};
+  const auto sine_theta{std::sinf(angle[0])};
+  const auto cosine_phi{std::cosf(angle[1])};
+  const auto sine_phi{std::sinf(angle[1])};
   return {sine_phi * cosine_theta * scale,
           sine_phi * sine_theta * scale,
           cosine_phi * scale,
