10fe06bf352ceb94792e5c340ee2f851930ccbda
zanadoman <domanseven@gmail.com>
2025-02-10 20:33:16 +0100

    removed junk from example main

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,21 +2,12 @@
 
 #include <cstdint>
 #include <iostream>
-#include <thread>
 
 namespace wzm = wizard_engine::modules;
 
 auto main([[maybe_unused]] std::int32_t argc,
           [[maybe_unused]] char* argv[]) noexcept -> std::int32_t {
   wzm::timer::get().set_frame_time(10);
-
-  const std::thread thread{[]() {
-    for (;;) {
-      wzm::timer::get().set_frame_time(255);
-      std::cout << wzm::timer::get_current_time() << "t\n";
-      wzm::timer::get().synchronize();
-    }
-  }};
 
   for (;;) {
     std::cout << wzm::timer::get_current_time() << '\n';
